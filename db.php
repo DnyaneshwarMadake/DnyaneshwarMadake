5471a0ac1ad7423e03c4cd90b2b050ac2f6c8e90
Dnyaneshwar madake <87903257+DnyaneshwarMadake@users.noreply.github.com>
2021-09-11 20:35:06 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,12 @@
+<!--************************************Database Connection File*************************************************** -->
+
+
+<?php
+//  Host name
+// Database User name
+// Passward
+// Database Name
+
+$conn=mysqli_connect('localhost','id17525753_dnyaneshwarmadake','Dnya1234567@','id17525753_basicbanking') or die("Database connection failed");
+
+?>
